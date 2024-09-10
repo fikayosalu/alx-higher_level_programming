2c0500120be0f6f642cdc8652b750f96b8b2611d
fikayosalu <salufikayo@gmail.com>
2024-09-10 23:15:40 +0100

    Fix task 1 errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if number > 0:
     elif lastdigit == 0:
         print(f"Last digit of {number:d} is {lastdigit:d} and is 0")
     elif lastdigit < 6 and lastdigit != 0:
-        print(f"Last digit of {number:d} is {lastdigit:d} \
+        print(f"Last digit of {number:d} is {lastdigit:d}\
  and is less than 6 and not 0")
 elif number <= 0:
     lastdigit = (number * -1) % 10
@@ -17,5 +17,5 @@ elif number <= 0:
     if lastdigit == 0:
         print(f"Last digit of {number:d} is {lastdigit:d} and is 0")
     elif lastdigit < 6 and lastdigit != 0:
-        print(f"Last digit of {number:d} is {lastdigit:d} \
+        print(f"Last digit of {number:d} is {lastdigit:d}\
  and is less than 6 and not 0")
