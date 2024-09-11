1fb76090e3a2db623459104923834f322db4adb6
fikayosalu <salufikayo@gmail.com>
2024-09-11 20:53:14 +0100

    Fixing task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,9 @@
 def print_last_digit(number):
     if number < 0:
         lastdigit = (number * -1) % 10
-        lastdigit = lastdigit * -1
-        return lastdigit
-    elif number >= 0:
+    elif number == 0:
+        lastdigit = 0
+    else:
         lastdigit = number % 10
-        return lastdigit
+    print (lastdigit, end="")
+    return lastdigit
