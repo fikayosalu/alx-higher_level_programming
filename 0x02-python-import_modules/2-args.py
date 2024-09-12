8223ae77a1296c5451eae5e399b5aeb51113c65b
fikayosalu <salufikayo@gmail.com>
2024-09-12 23:40:08 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("1 argument")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments".format(len(argv)-1))
