766de0b13bee225eecc5ca9e5c3ddbe73688af99
fikayosalu <salufikayo@gmail.com>
2024-09-18 01:13:27 +0100

    Task 3 error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list) - 1
+
+    if len(my_list) == 0:
+        print()
     while length >= 0:
         print("{:d}".format(my_list[length]))
         length -= 1
