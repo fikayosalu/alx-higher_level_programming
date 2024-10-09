7b7ccc229679a5649e7dd3888e2a3f184c8de508
fikayosalu <salufikayo@gmail.com>
2024-10-09 10:28:59 +0100

    Added string format to task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -12,8 +12,11 @@ def uppercase(str):
     prints a string in uppercase
     """
 
+    new_str = ""
+
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            i = chr(ord(i) - 32)
-        print("{}".format(i), end="")
-    print("".format())
+            new_str += chr(ord(i) - 32)
+        else:
+            new_str += i
+    print("{}".format(new_str))
