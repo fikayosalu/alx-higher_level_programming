85aff80eb689434da72d1c9c74b96cf99ce7266d
fikayosalu <salufikayo@gmail.com>
2024-10-11 07:17:27 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/bin/usr/python3
+"""
+1.square module
+OOP Programming style 
+Contains a class Square
+"""
+
+
+class Square:
+    """
+    Has a private attribute "size"
+    An object instance "size" white no type/value
+    """
+
+    def __init__(self, size):
+        """
+        constructor __init__ has gives the object instance
+        attribute when created
+        It contains the size attribute
+        """
+
+        self.__size = size
