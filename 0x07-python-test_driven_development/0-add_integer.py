369200771b86b28e2027e6778fc331120703f837
fikayosalu <salufikayo@gmail.com>
2024-10-13 23:58:37 +0100

    Task 0 test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,7 +20,7 @@ def add_integer(a, b=98):
     """
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return int(a) + int(b)
-    if not isinstance(a, (int, float)):
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
