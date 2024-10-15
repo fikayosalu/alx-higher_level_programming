11a993236da47e428c495e4e3a6b4fd997ff02ad
fikayosalu <salufikayo@gmail.com>
2024-10-16 00:56:48 +0100

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+6-load_from_json_file module
+Contains a function save_to_json_file
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Writes an Object to a text file, using a JSON representation
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+       return json.loads(file.read())
