22b8ec1c74ac4549507653a69c8f8d94db40bdc6
fikayosalu <salufikayo@gmail.com>
2024-10-15 23:03:46 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """
     Reads a text file and prints it to stdout
     """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read())
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
