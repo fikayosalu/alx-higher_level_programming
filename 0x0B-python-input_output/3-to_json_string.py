02ab796ddbc63fcdcce518a4d27431c0cb04cf6d
fikayosalu <salufikayo@gmail.com>
2024-10-15 23:52:42 +0100

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+3-to_json_string module
+Contains a function to_json_string
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object(string)
+    """
+    return json.dumps(my_obj)
