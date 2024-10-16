8e4ca38360ced9b69d8872819c6fe20332080bb2
fikayosalu <salufikayo@gmail.com>
2024-10-16 19:42:56 +0100

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,18 +15,17 @@ def save_arg_in_json():
     """
     Writes an Object to a text file, using a JSON representation
     """
-    args = []
-    save_to_json_file(args, "add_item.json")
-    save_to_json_file(history, "add_item.json")
-    i = 1
-    print(history)
-    if len(argv) > 1:
-        while i < len(argv):
-            if argv[i] not in history:
-                history.append(argv[i])
-            i += 1
-    print(history)
-    save_to_json_file(history, "add_item.json")
+    filename = "add_item.json"
+
+    try:
+        args = load_from_json_file(filename)
+
+    except FileNotFoundError:
+        args = []
+        save_to_json_file(args, filename)
+
+    args.extend(argv[1:])
+    save_to_json_file(args, filename)
 
 
 save_arg_in_json()
