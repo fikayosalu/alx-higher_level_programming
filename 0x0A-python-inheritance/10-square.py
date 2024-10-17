a0b8416416052574548451eb5ce5452d12c3bfb0
fikayosalu <salufikayo@gmail.com>
2024-10-18 00:24:29 +0100

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+"""
+10-square module
+Contains a class Square
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
+    """
+    Defines a square
+    Inherits from the class Rectangle
+    """
     def __init__(self, size):
+        """
+        Initializes instance of Square with attributes
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
