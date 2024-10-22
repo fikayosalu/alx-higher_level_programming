30bc4327db9b64c75449a759fc38000f5ba43b7d
fikayosalu <salufikayo@gmail.com>
2024-10-22 14:12:05 +0100

    From Json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,17 @@ class Base:
         if list_dictionaries is None or []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string is []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        with open(f"{cls.__name__}.json", "w", encoding="utf-8") as file:
+            if list_objs is None:
+                file.write([])
+            list_dict = [r.__dict__ for r in list_objs]
+            file.write(Base.to_json_string(list_dict))
