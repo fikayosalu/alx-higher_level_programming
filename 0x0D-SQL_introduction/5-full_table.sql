29ae802d3bcd56bb74400c0a455b4e11823dc0b9
fikayosalu <salufikayo@gmail.com>
2024-11-20 01:48:25 +0100

    Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,9 @@
--- Query to print the full description of the first_table from the given database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+-- Get the full description of the first_table from the given database
+SELECT COLUMN_NAME, 
+       COLUMN_TYPE, 
+       IS_NULLABLE, 
+       COLUMN_DEFAULT, 
+       EXTRA 
 FROM information_schema.columns
-WHERE table_schema = DATABASE()
-AND table_name = 'first_table';
+WHERE table_schema = 'hbtn_0c_0' 
+  AND table_name = 'first_table';
