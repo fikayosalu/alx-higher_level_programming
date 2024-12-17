71bc3e165b2f407259a0aa1ffe68f4987c2716c0
fikayosalu <salufikayo@gmail.com>
2024-12-17 12:49:51 +0100

    Didn't use the length method

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argumentArr = process.argv;
-if (argumentArr.length <= 2) {
-  console.log('No argument');
-} else {
+if (argumentArr[2]) {
   console.log(argumentArr[2]);
+} else {
+  console.log('No argument');
 }
