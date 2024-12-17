eca6422393dd9ad66a7525d2c890d66894d8bbed
fikayosalu <salufikayo@gmail.com>
2024-12-17 12:17:04 +0100

    command-line args

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-let myVar = 'C is fun';
-console.log(myVar);
-myVar = 'Python is cool';
-console.log(myVar);
-myVar = 'Javascript is amazing';
-console.log(myVar);
+console.log('C is fun', 'Python is cool', 'Javascript is amazing');
