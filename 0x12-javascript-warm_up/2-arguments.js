525c5df9a50814baa6535c30642310c9319427e0
fikayosalu <salufikayo@gmail.com>
2024-12-17 12:34:53 +0100

    Fixed standard error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argumentArr = process.argv;
-if (argumentArr.length <= 2){
-	console.log('No argument')
-}else if(argumentArr.length === 3){
-	console.log('Argument found')
-}else{
-	console.log('Arguments found')
+if (argumentArr.length <= 2) {
+  console.log('No argument');
+} else if (argumentArr.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
