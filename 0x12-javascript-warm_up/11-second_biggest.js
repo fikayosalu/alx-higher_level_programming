11993d643d10566c443399f4ac8f65e3f9a25e6b
fikayosalu <salufikayo@gmail.com>
2025-01-09 01:44:30 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,11 @@ const argumentArr = process.argv;
 let max = Number.NEGATIVE_INFINITY;
 let secondMax = Number.NEGATIVE_INFINITY;
 let i = 2;
-let j = 2;
 if (argumentArr.length <= 3) {
   console.log(0);
 } else {
   while (i < argumentArr.length) {
-    let num = Number(argumentArr[i]);
+    const num = Number(argumentArr[i]);
     if (num > max) {
       secondMax = max;
       max = num;
