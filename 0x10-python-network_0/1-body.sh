6c7f3f63d4df66ff8fe15f58b968dc59b0f36133
fikayosalu <salufikayo@gmail.com>
2025-01-17 02:36:58 +0100

    status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to get the size of the response body in bytes
-curl -s -o response.txt -w "%{http_code}" "$1" | grep -q "200" && cat response.txt
+[ "$(curl -s -o response.txt -w "%{http_code}" "$1")" -eq 200 ] && cat response.txt
