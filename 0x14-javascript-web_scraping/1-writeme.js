9e813ad5e28b00e8464dac24cdc3d8b56fd791f7
fikayosalu <salufikayo@gmail.com>
2025-02-11 23:43:57 +0100

    Write file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 const file = process.argv[2];
 const data = process.argv[3];
 
+if (!file || data === undefined) {
+  console.error('Usage: node 1-writeme.js <filename> <content>');
+  process.exit(1);
+}
+
 fs.writeFile(file, data, function (err) {
   if (err) throw err;
-  console.log('file successfully created');
 });
