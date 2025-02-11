07deb6878371a998ef1d1c43046abb6b5ccdb5a2
fikayosalu <salufikayo@gmail.com>
2025-02-11 19:58:37 +0100

    Reading a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) throw err;
+  //console.log(data);
+  console.log(data);
+});
