78f043100aeb0e11f805a5c15d352e04a0aade87
fikayosalu <salufikayo@gmail.com>
2025-02-19 23:49:33 +0100

    Task 9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+/* global $ */
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    data.results.forEach(movie => {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
+});
